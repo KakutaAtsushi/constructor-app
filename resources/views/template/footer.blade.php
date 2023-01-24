603c1802fb22392c39cae2a63af24aae3a6c58b7
Atsushi <kakuta.atsushi@outward.jp>
2023-01-25 01:06:37 +0900

    fdew:;ed git push origi
    ;

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -17,7 +17,7 @@
                     Push.close('myTag');
                     new Promise((resolve) => {
                         axios.get('https://shyu-web.sakura.ne.jp/public/remind').then(response => {
-                            console.debug(response.data)
+                            console.debug(typeof response.data)
                             if (response.data !== "false") {
                                 Push.create("工事の三日前になりました", {
                                     body: response.data["location"],
