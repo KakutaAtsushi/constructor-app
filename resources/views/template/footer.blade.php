c3a334a5734b5ba732f6b608c4f4df05720b5e31
Atsushi <kakuta.atsushi@outward.jp>
2023-01-26 16:56:14 +0900

    footer tiny ifx

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -3,15 +3,14 @@
 <script src="https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js"></script>
 <script>
     construct_notify = () => {
-            axios.get('https://shyu-web.sakura.ne.jp/public/remind').then(async response => {
-                console.error("success")
-            }).catch(error => {
-                console.log(error);
-            })
-    }
-            setInterval(async () => {
-                await construct_notify()
-            }, 1000)
+        axios.get('https://shyu-web.sakura.ne.jp/public/remind').then(async response => {
+            console.error("success")
+        }).catch(error => {
+            console.log(error);
         })
+    }
+    setInterval(async () => {
+        await construct_notify()
+    }, 1000)
 </script>
 </html>
