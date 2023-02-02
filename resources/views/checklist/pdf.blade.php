b23b0c0af28ce15f708cc1acac685d9de6b0820a
Atsushi <kakuta.atsushi@outward.jp>
2023-02-02 09:00:27 +0900

    test

--- resources/views/checklist/pdf.blade.php
+++ resources/views/checklist/pdf.blade.php
@@ -18,9 +18,9 @@
         <div>
             <p style="margin:5px">タイトル:{{$title}}</p>
             @foreach($items as $key => $item)
-                <div style="height: 20px; ">
-                    <div style="margin:1px; padding:1px" class="pdf">
-                        <p>項目{{$key+1}}: @if($item === "")無し@else{{$item}}@endif</p>
+                <div style="height: 20px;">
+                    <div style="margin:1px; padding:1px">
+                        <p  class="pdf">項目{{$key+1}}: @if($item === "")無し@else{{$item}}@endif</p>
                     </div>
                 </div>
             @endforeach
