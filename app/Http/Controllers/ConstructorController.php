9caa9dd1df2b7a66ed48580174e1b037302b3923
Atsushi <kakuta.atsushi@outward.jp>
2023-02-02 09:06:50 +0900

    fe

--- app/Http/Controllers/ConstructorController.php
+++ app/Http/Controllers/ConstructorController.php
@@ -164,7 +164,7 @@ class ConstructorController extends Controller
                 array(
                     $field,
                 ),
-                $url = "https:shyu-web.sakura.ne.jp/public/construct/edit/$id",
+                $url = "/public/construct/edit/$id",
                 $data = null,
                 $buttons = null,
                 $schedule = null
