b3274a8500460f56137e34d934d2e42a96698c7d
Atsushi <kakuta.atsushi@outward.jp>
2023-02-03 11:54:39 +0900

    fix

--- resources/views/construct/index.blade.php
+++ resources/views/construct/index.blade.php
@@ -48,7 +48,7 @@
             </tbody>
         </table>
         <div class="mt-1 mb-1 row justify-content-center">
-            {{ $board_list->links() }}
+            {{ $construct->links() }}
         </div>
     </div>
 </div>
