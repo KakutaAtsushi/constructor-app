277cd826925953934fc869128724af8e906dd63e
Atsushi <kakuta.atsushi@outward.jp>
2023-02-14 23:55:01 +0900

    add migration file

---
+++ database/migrations/2023_02_14_235419_add_news_to_constructs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewsToConstructsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('constructs', function (Blueprint $table) {
+            $table->string("news");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('constructs', function (Blueprint $table) {
+            //
+        });
+    }
+}
