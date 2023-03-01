670c4fe4138bd1fce72ef01116bd3f7125416108
KakutaAtsushi <kakuta.atsushi0909@gmail.com>
2023-03-01 15:03:25 +0900

    test

--- app/Http/Controllers/ConstructorController.php
+++ app/Http/Controllers/ConstructorController.php
@@ -77,7 +77,7 @@ class ConstructorController extends Controller
         if ($office_name != "無し") {
             $fields = $this->create_fields($office_name);
         }
-        $googlemapURL = "https://www.google.co.jp/maps/@".$form_items['coordinate']."19.25z?hl=ja" ?? $form_items['location']."19.25z?hl=ja";
+        $googlemapURL = "https://www.google.com/maps?q=".$form_items['coordinate'] ?? "https://www.google.com/maps?q=".$form_items['location'];
         $route_name = $this->processing_route_name($form_items);
         $data = Constructor::create(["location" => $form_items["location"], "google_map_url" => $googlemapURL, "username" => $form_items["username"], "department" => $form_items["department"], "business_name" => $form_items["business_name"], "route" => $route_name, "real_work_time" => $form_items["real_work"], "bus_station"
         => $form_items["bus_station"], "news" => $form_items["news"], "inworking_start_time" => $form_items["inworking_start_time"], "inworking_end_time" => $form_items["inworking_end_time"],"notify_time" => $form_items["notify_time"], "coordinate" => $form_items["coordinate"], "stopped_bus_flag" => $form_items["stopped_bus"] ?? 0, "detour_flag" => $form_items["detour"] ?? 0, "bus_relocation_flag" => $form_items["relocation_bus"] ?? 0, "remarks" => $form_items["remarks"], "flag" => 0, "office" => $office_name ?: "無し", "detail" => $form_items["detail"], "started_at" => $form_items["start"], "ended_at" =>
@@ -112,7 +112,7 @@ class ConstructorController extends Controller
         if ($fields != []) {
             $this->send_target($fields, $form_items["location"] . "が更新されました。", $construct_id);
         }
-        $googlemapURL = "https://www.google.co.jp/maps/@".$form_items['coordinate']."19.25z?hl=ja" ?? $form_items['location']."19.25z?hl=ja";
+        $googlemapURL = "https://www.google.com/maps?q=".$form_items['coordinate'] ?? "https://www.google.com/maps?q=".$form_items['location'];
         Constructor::where("id", $construct_id)->update(["remarks" => $form_items["remarks"], "google_map_url" => $googlemapURL, "inworking_start_time"=>$form_items["inworking_start_time"],"inworking_end_time"=>$form_items["inworking_end_time"],  "news" => $form_items["news"],"location" => $form_items["location"], "notify_time" => $form_items["notify_time"], "coordinate" => $form_items["coordinate"], "stopped_bus_flag" => $form_items["stopped_bus"] ?? 0, "bus_relocation_flag" => $form_items["relocation_bus"] ?? 0, "detour_flag" => $form_items["detour"] ?? 0, "office" => $offices, "real_work_time" => $form_items["real_work"] ?? "", "detail" => $form_items["detail"], "started_at" => $form_items["started_at"], "ended_at" => $form_items["ended_at"]]);
         return redirect("/construct/edit/" . $construct_id);
     }
